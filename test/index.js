5d538dff322e5fa0959d77af45d873623066c052
Aaron Eiche <aaron@aaroneiche.com>
2019-06-20 18:55:43 -0700

    Adds tests for article GET endpoints

--- test/index.js
+++ test/index.js
@@ -7,3 +7,40 @@ var chaiHttp = require('chai-http');
 var server = require('../index');
 
 
+/* 
+Things we need to test (per spec):
+GET article
+Accepts ID, returns object {id, title, date, body, categories}
+
+POST article
+Creates article with title, body, categories, returns id, datetime
+*/
+
+chai.use(chaiHttp);
+
+describe("API Routes", ()=>{
+
+    it("Fails to get an Article by ID", ()=>{
+        chai.request(server)
+        .get("/articles/1")
+        .send()
+        .end(function(err,res){
+          res.should.be.json;
+          res.should.have.status(404);
+          done();
+        })
+    });
+
+    it("Gets an Article by ID", ()=>{
+        chai.request(server)
+        .get("/articles/1")
+        .send()
+        .end(function(err,res){
+          res.should.be.json;
+          res.should.have.status(200);
+          res.body.id.should.equal(1);
+          done();
+        })
+    });
+
+})
