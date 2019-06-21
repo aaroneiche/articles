4f374f2dc36b372c579bd997a2cab561db5ccace
Aaron Eiche <aaron@aaroneiche.com>
2019-06-20 18:46:19 -0700

    Initial commit. Basic server and roots of test

---
+++ test/index.js
@@ -0,0 +1,9 @@
+var mocha = require('mocha');
+var chai = require('chai');
+var should = chai.should();
+var fs = require("fs");
+
+var chaiHttp = require('chai-http');
+var server = require('../index');
+
+
